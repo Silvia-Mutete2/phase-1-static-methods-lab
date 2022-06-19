960e6311c57204d0a302225c01d610803467341f
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-19 10:24:43 +0300

    Done

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
-}
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, "")
+  }
+
+  
+  static titleize(title){
+    if(!title) {
+        return '';
+      }
+      let arr = [];
+      let excludedWords = ['of', 'the', 'and', 'a', 'an', 'but', 'for', 'at', 'by', 'from']
+      arr = title.split(' ');
+      return arr.map((word, i) =>
+      {
+         return excludedWords.includes(word) && i > 0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+    
+      }).join(' ');
+    }
+    }
+    
+ 
